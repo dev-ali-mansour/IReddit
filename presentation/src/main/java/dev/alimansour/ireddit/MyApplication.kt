16378cb692fc747046374fb3d3af2215d9bf11d3
Ali Mansour <dev.ali.mansour@gmail.com>
2021-11-12 13:01:53 +0200

    Add Timber library as shared dependency in build.gradle of data module. Override onCreate() of MyApplication to configure Timber library to show logs for debug builds only.

--- presentation/src/main/java/dev/alimansour/ireddit/MyApplication.kt
+++ presentation/src/main/java/dev/alimansour/ireddit/MyApplication.kt
@@ -3,9 +3,17 @@ package dev.alimansour.ireddit
 import androidx.multidex.MultiDexApplication
 import dev.alimansour.ireddit.di.AppComponent
 import dev.alimansour.ireddit.di.DaggerAppComponent
+import timber.log.Timber
 
 class MyApplication : MultiDexApplication() {
     val appComponent: AppComponent by lazy {
         DaggerAppComponent.builder().context(applicationContext).build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
