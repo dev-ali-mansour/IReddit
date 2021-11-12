40935698cf7e83948ca77b20a079d5ef46dfcf89
Ali Mansour <dev.ali.mansour@gmail.com>
2021-11-12 21:40:20 +0200

    Create FavoritesViewModelFactory. Create GetFavoritesUseCase. Update return type of action methods in PostsRepository and PostsRepositoryImpl to LiveData<Resource<String>>. Add GetFavoritesUseCase, RemovePostFromFavoriteUseCase, ClearFavoriteUseCase, DisposeObserversUseCase as dependencies of FavoritesViewModel and FavoritesViewModelFactory. Add LiveData objects for FavoritesViewModel. Add getFavorites() in FavoritesViewModel. Add removeFromFavorites() in FavoritesViewModel. Add clearFavorites() in FavoritesViewModel. Override onCleared() in FavoritesViewModel to clear observers. Add provideFavoritesViewModel() to provide FavoritesViewModel in ViewModelModule. Update the scope of functions in ViewModelModule to ActivityScope. Override onAttach() of FavoritesFragment to inject it to ViewModelComponent. Inject FavoritesViewModel in FavoritesFragment.

--- presentation/src/main/java/dev/alimansour/ireddit/di/ViewModelModule.kt
+++ presentation/src/main/java/dev/alimansour/ireddit/di/ViewModelModule.kt
@@ -4,17 +4,27 @@ import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
 import dagger.Module
 import dagger.Provides
+import dev.alimansour.ireddit.ui.favorites.FavoritesViewModel
+import dev.alimansour.ireddit.ui.favorites.FavoritesViewModelFactory
 import dev.alimansour.ireddit.ui.home.HomeViewModel
 import dev.alimansour.ireddit.ui.home.HomeViewModelFactory
 
 @Module
 object ViewModelModule {
 
+    @ActivityScope
     @Provides
     fun provideHomeViewModel(
         activity: FragmentActivity,
         homeViewModelFactory: HomeViewModelFactory
-    ): HomeViewModel {
-        return ViewModelProvider(activity, homeViewModelFactory)[HomeViewModel::class.java]
-    }
+    ): HomeViewModel =
+        ViewModelProvider(activity, homeViewModelFactory)[HomeViewModel::class.java]
+
+    @ActivityScope
+    @Provides
+    fun provideFavoritesViewModel(
+        activity: FragmentActivity,
+        favoritesViewModelFactory: FavoritesViewModelFactory
+    ): FavoritesViewModel =
+        ViewModelProvider(activity, favoritesViewModelFactory)[FavoritesViewModel::class.java]
 }
